624005e60cfe43ce6def1801362582df760d860a
araujo-alef <alefaraujo.dev@gmail.com>
2021-09-11 01:07:01 -0400

    added top bar on page home

--- lib/app/ui/android/home/home_page.dart
+++ lib/app/ui/android/home/home_page.dart
@@ -4,10 +4,88 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height / 100;
+    final width = MediaQuery.of(context).size.width / 100;
     return Scaffold(
-      body: Center(
-        child: Text('Dine Market'),
-      ),
+      backgroundColor: Color(0XFFF6F7FC),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 8.0, left: 30, right: 30),
+          child: Column(
+            children: [
+              // top bar
+              Container(
+                height: height * 4.5,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: width * 39,
+                      height: height * 2.9,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/logo.png'),
+                        )
+                      ),
+                    ),
+                    Container(
+                      child: Container(
+                        height: height * 4.5,
+                        width: height * 4.5,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/user_profile.png')
+                          )
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: height * 5,),
+              Container(
+                height: height * 5.3,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: height * 5.3,
+                      width: width * 70,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                      ),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: height * 5.3,
+                            width: height * 5.3,
+                            child: TextButton(
+                              onPressed: () {},
+                              child: Padding(
+                                padding: const EdgeInsets.all(7.0),
+                                child: Image(image: AssetImage('assets/images/search.png'),),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: height * 5.3,
+                      width: height * 5.3,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      )
     );
   }
 }

a9e5a32d562b7cc8efc6f40af6a0f4883ba1d86a
araujo-alef <alefaraujo.dev@gmail.com>
2021-09-12 01:35:20 -0400

    added action chip Fresh on home page

--- lib/app/ui/android/home/home_page.dart
+++ lib/app/ui/android/home/home_page.dart
@@ -45,63 +45,93 @@ class HomePage extends StatelessWidget {
               ),
               SizedBox(height: height * 5,),
               Container(
-                height: height * 5.3,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                child: Column(
                   children: [
-                    Container(
-                      height: height * 5.3,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(Radius.circular(10))
-                      ),
-                      child: Row(
-                        children: [
-                          Container(
-                            height: width * 11.5,
-                            width: width * 11.5,
-                            child: TextButton(
-                              onPressed: () {},
-                              child: Padding(
-                                padding: const EdgeInsets.all(7.2),
-                                child: Image(image: AssetImage('assets/images/search.png'),),
-                              ),
-                            ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: width * 11.5,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(Radius.circular(10))
                           ),
-                          Container(
-                            width: width * 60,
-                            child: TextField(
-                              decoration: InputDecoration(
-                                hintText: 'Search',
-                                hintStyle: TextStyle(
-                                  color: Color(0XFF212121),
+                          child: Row(
+                            children: [
+                              Container(
+                                height: width * 11.5,
+                                width: width * 11.5,
+                                child: TextButton(
+                                  onPressed: () {},
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(7.2),
+                                    child: Image(image: AssetImage('assets/images/search.png'),),
+                                  ),
                                 ),
-                                border: InputBorder.none,
                               ),
-                              style: GoogleFonts.poppins(
-                                fontWeight: FontWeight.w500,
-                                fontSize: width * 3.5
+                              Container(
+                                width: width * 60,
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    hintText: 'Search',
+                                    hintStyle: TextStyle(
+                                      color: Color(0XFF212121),
+                                    ),
+                                    border: InputBorder.none,
+                                  ),
+                                  style: GoogleFonts.poppins(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: width * 3.5
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                                width: width * 11,
+                                height: width * 11,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                                ),
+                                child: ElevatedButton(
+                                    onPressed: () {},
+                                    child: Image(image: AssetImage('assets/images/sliders_icon.png')),
+                                    style: TextButton.styleFrom(
+                                      backgroundColor: Color(0XFF212121),
+                                    ),
+                                  ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: height * 1,),
+                    Container(
+                      height: height * 2.2,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          Wrap(
+                            spacing: 6,
+                            children: [
+                              Chip(
+                                label: Text('Fresh'),
+                                onDeleted: () {},
+                                deleteIcon: Icon(Icons.close_sharp, size: height * 1.3, color: Colors.white,), 
+                                backgroundColor: Color(0XFF212121),
+                                labelStyle: GoogleFonts.poppins(
+                                  fontSize: width * 2.5,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w600
+                                ),
+                                visualDensity: VisualDensity.compact,
+                                labelPadding: EdgeInsets.only(left: 8, right: 0, top: 0, bottom: 0),
                               ),
-                            ),
+                              
+                            ],
                           )
                         ],
                       ),
-                    ),
-                    Container(
-                            width: width * 11,
-                            height: height * 5.5,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.all(Radius.circular(10)),
-                              color: Color(0XFF212121),
-                            ),
-                            child: TextButton(
-                                onPressed: () {},
-                                child: Padding(
-                                  padding: const EdgeInsets.all(6),
-                                  child: Image(image: AssetImage('assets/images/sliders_icon.png')),
-                                ),
-                            ),
-                          ),
+                    )
                   ],
                 ),
               )

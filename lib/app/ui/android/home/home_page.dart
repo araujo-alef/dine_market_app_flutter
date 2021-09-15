318869b770505b14ba956b6928d750c674ff8ee9
araujo-alef <alefaraujo.dev@gmail.com>
2021-09-14 22:17:46 -0400

    added slider card on home page

--- lib/app/ui/android/home/home_page.dart
+++ lib/app/ui/android/home/home_page.dart
@@ -1,7 +1,13 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
+
+  final assetsImages = [
+    'assets/images/image_card.png',
+    'assets/images/image_card.png',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -146,6 +152,30 @@ class HomePage extends StatelessWidget {
                     )
                   ],
                 ),
+              ),
+              SizedBox(height: height * 4,),
+              Container(
+                decoration: BoxDecoration(
+                color: Color(0XFF212121),
+                borderRadius: BorderRadius.all(Radius.circular(10))
+              ),
+                height: height * 11,
+                child: CarouselSlider.builder(
+                  itemCount: assetsImages.length,
+                  options: CarouselOptions(
+                    height: height * 11,
+                    viewportFraction: 1,
+                    enableInfiniteScroll: false,
+                  ),
+                  itemBuilder: (context, index, realIndex) {
+                    final assetImage = assetsImages[index];
+                    final w = width;
+                    final h = height;
+
+                    return buildCard(assetImage, index, w, h);
+                  },
+                ),
+                
               )
             ],
           ),
@@ -153,4 +183,59 @@ class HomePage extends StatelessWidget {
       )
     );
   }
+}
+
+Widget buildCard(String assetImage, int index, w, h) {
+  return Container(
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.all(Radius.circular(10)),
+    ),
+    child: Padding(
+      padding: const EdgeInsets.only(left: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: Text(
+                    'Fresh Products',
+                    textAlign: TextAlign.start,
+                    style: GoogleFonts.berkshireSwash(
+                      fontSize: w * 6,
+                      color: Colors.white,
+                    )
+                  ),
+                ),
+                SizedBox(height: 3,),
+                Container(
+                  child: Text(
+                    'Always provide best quality\ningredients',
+                    textAlign: TextAlign.justify,
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w500,
+                      fontSize: w * 2.6,
+                      color: Colors.white,
+                    )
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              image: DecorationImage(
+                image: AssetImage(assetImage),
+                fit: BoxFit.cover
+              )
+            ),
+            width: w * 24,
+          ),
+        ],
+      ),
+    ),
+  );
 }
